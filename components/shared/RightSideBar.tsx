685b92699e15ce693e51e010bd2edf8bfbbb814c
Vs-kishu <91000916+Vs-kishu@users.noreply.github.com>
2023-08-24 18:19:18 +0530

    Authentication logic builded

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -3,7 +3,11 @@ const RightSideBar = () => {
   return (
     <section className="custom-scrollbar rightsidebar">
       <div className="flex flex-1 flex-col justify-start">
-        <h3>suggested commuties</h3>
+        <h3 className="text-heading4-medium text-light-2">suggested commuties</h3>
+
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-heading4-medium text-light-2">suggested commuties</h3>
 
       </div>
     </section>
